e9021ba80a003c29c2c95e4a8c5760de796b757e
vlad Amirov <vladamirov@list.ru>
2024-05-26 20:32:17 +0300

    docker-compose add

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD')
+
     }
 }
 
