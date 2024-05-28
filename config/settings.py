31a927f23506a19a6e48abcf934cf1a0476c5353
Vlad_work_oficie <vladamirov@list.ru>
2024-05-28 12:42:58 +0300

    validate add

--- config/settings.py
+++ config/settings.py
@@ -68,8 +68,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD')
-
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
